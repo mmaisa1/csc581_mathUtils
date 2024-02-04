3baaadb1c57fc2c2544b7aaf02613319a0208c78
mmaisa1 <mmaisa1@toromail.csudh.edu>
2024-02-04 14:11:52 -0800

    renaming mathUtils test

--- test_math_utils.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import MathUtils
-
-class testMathUtils(unittest.TestCase):
-    def testClass(self):
-       o = MathUtils.MathUtils()
-       assert o.add()== 5
-
-unittest.main()
